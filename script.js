6219a2a57d9169be379d92cff1cf08be2c8e6354
Ra1v00 <79638847+Ra1v00@users.noreply.github.com>
2023-03-29 17:35:46 +0700

    Yandex map
    Добавил скролящийся header и яндекс карту. Немного позже кину регистрационную страницу

--- script.js
+++ script.js
@@ -14,46 +14,48 @@ const body = document.querySelector("body");
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   menu.classList.toggle("active");
-  body.classList.toggle("lock");
+  /*body.classList.toggle("lock");*/
 });
 
 
 /*services_content_column_button1*/
-function First()
-{
-const btn = document.getElementById('btn');
-btn.addEventListener('click', () => 
+function First() 
 {
   window.location.assign('First.html');
-});
 }
+const btn = document.getElementById('btn');
+btn.addEventListener('click', First);
 
 /*services_content_column_button2*/
 function Second()
 {
-const btn = document.getElementById('btn2');
-btn.addEventListener('click', () => 
-{
   window.location.assign('Second.html');
-});
 }
+const btn2 = document.getElementById('btn2');
+btn.addEventListener('click', Second);
 
 /*services_content_column_button3*/
 function Third()
 {
-const btn = document.getElementById('btn3');
-btn.addEventListener('click', () => 
-{
   window.location.assign('Third.html');
-});
 }
+const btn3 = document.getElementById('btn3');
+btn.addEventListener('click', Third);
 
 /*services_content_column_button4*/
 function Fourth()
 {
-const btn = document.getElementById('btn4');
-btn.addEventListener('click', () => 
-{
   window.location.assign('Fourth.html');
-});
 }
+const btn4 = document.getElementById('btn4');
+btn.addEventListener('click', Fourth);
+
+/*Яндекс карта*/
+ymaps.ready(init);
+function init() {
+    var myMap = new ymaps.Map("navigation", {
+        center: [55.76, 37.64], // координаты центра карты
+        zoom: 10 // уровень масштабирования
+    });
+}
+ymaps.ready(init);

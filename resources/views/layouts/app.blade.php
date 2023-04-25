6aaff9aa8aa896329424c7ea6fb857a2c0f34e42
Ra1v00 <79638847+Ra1v00@users.noreply.github.com>
2023-04-25 17:39:06 +0700

    17:38
    минимальные правки

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield("title")</title>
-        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="design.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css>
             <!-- Fonts -->
             <link rel="dns-prefetch" href="//fonts.gstatic.com">
